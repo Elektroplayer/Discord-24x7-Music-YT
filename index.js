236e3d36172611cf33cd01f365577b37ac30ea14
Navaneeth K M <34748927+navaneethkm004@users.noreply.github.com>
2021-02-05 11:08:15 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,28 @@
+const { TOKEN, CHANNEL_ID, SERVER_ID, YT_LINK } = require("./config.json");
+const { STATUS, STATUS_TYPE, STATUS_URL } = require("./status.json")
+const discord = require("discord.js");
+const client = new discord.Client();
+const ytdl = require('ytdl-core');
+
+client.on('ready', async () => {
+  console.log("Started streaming " + (YT_LINK) + ` as ${client.user.tag}`);
+  client.user.setActivity((STATUS),{ type: (STATUS_TYPE),
+  url: (STATUS_URL)});
+  let channel = client.channels.cache.get(CHANNEL_ID) || await client.channels.fetch(CHANNEL_ID)
+
+  if(!channel) return;
+  const connection = await channel.join();
+  connection.play(ytdl(YT_LINK))
+})
+
+setInterval(async function() {
+  if(!client.voice.connections.get(SERVER_ID)) {
+    let channel = client.channels.cache.get(CHANNEL_ID) || await client.channels.fetch(CHANNEL_ID)
+    if(!channel) return;
+
+    const connection = await channel.join()
+    connection.play(ytdl(YT_LINK))
+  }
+}, 20000)
+
+client.login(TOKEN)
